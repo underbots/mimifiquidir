428b44d457a764e75bb4bc51d0a00ad538a0841d
Blanca <blancacano2.71828@gmail.com>
2018-06-30 13:48:13 +0200

    mimifiquidir_bot fino y mimimifiquidir scrip.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 
-TOKEN = ""
+TOKEN = "614932998:AAHb-z8szBE-IU4oVlcKGWVogq4VflRV9_0"
---
+++ mimificador_bot.py
@@ -0,0 +1,51 @@
+# https://python-telegram-bot.org/
+# Hili, qui pisi
+
+from telegram.ext import Updater, CommandHandler
+from config import TOKEN 
+
+def mimi( ms , indx = 0 ):
+    """ str -> str
+"""
+    #caracteres especiales
+    #que -> qui (la u no te la puede transformar )
+    #c[aou] -> qui
+    #g[aeou] -> gui
+    # estos casos han sido generados con un for de antemano
+    esp = [ ["que" , "qui"] , ['ca', 'qui'], ['co', 'qui'], ['cu', 'qui'],['ga', 'gui'], ['ge', 'gui'], ['go', 'gui'], ['gu', 'gui'], 
+                ['QUE', 'QUI'], ['CA', 'QUI'], ['CO', 'QUI'], ['CU', 'QUI'], ['GA', 'GUI'], ['GE', 'GUI'], ['GO', 'GUI'], ['GU', 'GUI']] 
+    i = 0
+    while i < len(esp):
+        ind = ms.find( esp[i][0])
+        if ind != -1:
+            return mimi( ms[0:ind] , indx+1) + esp[i][1] + mimi(ms[ind+len(esp[i][0]):] , indx)
+        else:
+            i += 1
+    return mimi_normal( ms[0:])
+    
+            
+
+def mimi_normal (ms):
+    """ str -> stryt
+"""
+    #Letras a reemplazar y su remplazamiento
+    cadena = [ ["aeou" , "i"] , ["aeou".upper() , "I"] , [ "áéóú" , "í"]  , ["áéóú".upper() , "I" ] ]
+
+    for cad in cadena:
+        for vocal in cad[0]:
+            ms = ms.replace(vocal , cad[1])
+    return ms
+
+
+def mimifica(bot, update):
+    mimificado = mimi(update.message.reply_to_message.text)
+    update.message.reply_to_message.reply_text(mimificado)
+
+
+updater = Updater(TOKEN)
+
+updater.dispatcher.add_handler(CommandHandler('mimifica', mimifica))
+updater.dispatcher.add_handler(CommandHandler('mimi', mimifica))
+
+updater.start_polling()
+updater.idle()
